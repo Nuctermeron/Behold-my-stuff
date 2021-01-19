42ed53f6cc175fe7ed708358cc1504880e19bbc2
Nuctermeron <76808832+Nuctermeron@users.noreply.github.com>
2021-01-19 15:28:45 +0100

    Started working on new function, with ISS location notifier

--- Projects/Tools/ISS_Tracker/application.py
+++ Projects/Tools/ISS_Tracker/application.py
@@ -1,5 +1,20 @@
 from tkinter import *
 import requests
+import datetime
+
+
+"""Change only these parameters"""
+MY_LAT = 50.264893 #example latitude
+MY_LONG = 19.023781 #example longitude
+"""----------------------------"""
+
+MY_PARAMETERS = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+LONGITUDE_VALUE = 0.0
+LATITUDE_VALUE = 0.0
 
 
 class Application:
@@ -30,14 +45,29 @@ class Application:
         self.get_location_button = Button(text="Get Location", width=20, highlightthickness=0,
                                           command=self.iss_location)
         self.get_location_button.grid(column=0, row=4, columnspan=2, padx=10, pady=10)
+
         mainloop()
 
     def iss_location(self):
         response = requests.get(url="http://api.open-notify.org/iss-now.json")
         data = response.json()
-        longitude_value = data["iss_position"]["longitude"]
-        latitude_value = data["iss_position"]["latitude"]
+        global LONGITUDE_VALUE, LATITUDE_VALUE
+        LONGITUDE_VALUE = data["iss_position"]["longitude"]
+        LATITUDE_VALUE = data["iss_position"]["latitude"]
         self.latitude_entry.delete(0, END)
         self.longitude_entry.delete(0, END)
-        self.latitude_entry.insert(0, longitude_value)
-        self.longitude_entry.insert(0, latitude_value)
+        self.latitude_entry.insert(0, LONGITUDE_VALUE)
+        self.longitude_entry.insert(0, LATITUDE_VALUE)
+
+    # def is_iss_flying_over(self):
+    #     response = requests.get(url="http://api.sunrise-sunset.org/json", params=MY_PARAMETERS)
+    #     data = response.json()
+    #     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    #     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    #     self.iss_location()
+    #     if MY_LAT-5 <= LATITUDE_VALUE <= MY_LAT+5 and MY_LONG-5 <= LONGITUDE_VALUE <= MY_LONG+5:
+    #         return True
+
+
+
+
