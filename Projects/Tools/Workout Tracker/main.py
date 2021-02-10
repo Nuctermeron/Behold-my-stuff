2c5d28fa391b5f55dea056fa42139b6aaf5229f8
Nuctermeron <76808832+Nuctermeron@users.noreply.github.com>
2021-02-10 14:59:18 +0100

    Workout tracker added

---
+++ Projects/Tools/Workout Tracker/main.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+
+APP_ID =  '7fb5a0c3' #Your AppID
+API_KEY = '2a9d3805988f44afbed10285559cdf06' #Your API Key
+GENDER = 'male'
+WEIGHT_KG = '90'
+HEIGHT_CM = '179'
+AGE = '26'
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+sheet_endpoint = 'https://api.sheety.co/d139c8f1f6ed0fe1784f116499fa7d6f/kopiaMyWorkouts/workouts'
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
