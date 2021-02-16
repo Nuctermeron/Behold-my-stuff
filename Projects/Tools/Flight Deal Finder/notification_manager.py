3ac2b51ddaf6cfa7f54ad0ae7e4d0752236009d9
Nuctermeron <76808832+Nuctermeron@users.noreply.github.com>
2021-02-16 14:11:00 +0100

    Added flight search and Twilio API to deal finder

--- Projects/Tools/Flight Deal Finder/notification_manager.py
+++ Projects/Tools/Flight Deal Finder/notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+
+TWILIO_SID = #Twilio Acc
+TWILIO_AUTH_TOKEN = #Twilio token
+TWILIO_VIRTUAL_NUMBER = #Twilio virtual number
+TWILIO_VERIFIED_NUMBER = #Twilio verified number
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
